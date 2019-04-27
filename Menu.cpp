09852003100fa154cb34b9301261f133791f6031
Mateus <mateusarthur00@homtail.com>
2019-04-27 16:29:13 -0300

    Mira Arrumada, tiros em construção

--- Menu.cpp
+++ Menu.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <allegro.h>
+#include <allegro5/allegro.h>
 #include <time.h>
+#include <math.h>
 
 // Atributos da tela
 #define LARGURA_TELA 500
 #define ALTURA_TELA 1024
-
 #define GRAUS_PARA_ALLEGRO(x) x/1.40625  //conversão
+
+// Timers
+volatile int TimerOuvirPassos;
+volatile int TimerTiros;
+
+// Variáveis Globais
+
+const int NUM_BALAS = 30;
+const int TILESIZE = 50;
 
 // Estruturas
 
-/*struct obj {int wx, wy, x, y, w, h;};
-struct obj
+enum IDS {JOGADORES, PROJETIL, INIMIGOS};
+
+struct TPersonagem 
 {
-	playerA = {0,0,200,200,50,50},
-	tiro = {0,0,200,200,50,50}
-};*/
+	float x, y, z;
+};
+
+struct TProjeteis
+{
+	int ID;
+	float x;
+	float y;
+	float z;
+	int velocidade;
+	bool recaregando = false;
+	bool ativo = false;
+};
+
 
 // Mapas
-
-const int TILESIZE = 50;
 
 enum
 {
@@ -29,6 +49,8 @@ enum
 	LARVA = 3,
 	PEDRA = 4
 };
+
+// FUNÇÕES
 
 int **Carregar_Mapa(const char *nome_arquivo, int *linhas, int *colunas)
 {
@@ -84,16 +106,90 @@ void Libera_Mapa(int **mapa, int linhas)
 		free(mapa[i]);
 	free(mapa);
 }
-// Variáveis Globais
 
-volatile int timer;
-int angulo;
-
-void incrementa_timer()
+// PEGAR O ANGULO DA MIRA 
+TPersonagem Subtrair(float Mira_x, float Mira_y, float Mira_z, TPersonagem Jogador)
 {
-	timer++;
+	TPersonagem diferenca;
+	diferenca.x = Mira_x - Jogador.x;
+	diferenca.y = Mira_y - Jogador.y;
+	diferenca.z = Mira_z - Jogador.z;
+	return diferenca;
 }
-END_OF_FUNCTION(incrementa_timer)
+float Magnitude(TPersonagem vec)
+{
+	return sqrtf(vec.x*vec.x + vec.y + vec.y + vec.z*vec.z);
+}
+float Distancia(float Mira_x, float Mira_y, float Mira_z, TPersonagem Jogador)
+{
+	TPersonagem diferenca = Subtrair(Mira_x, Mira_y, Mira_z, Jogador);
+	return Magnitude(diferenca);
+}
+TPersonagem CalcularAngulo(float Mira_x, float Mira_y, float Mira_z, TPersonagem Jogador)
+{
+	TPersonagem diferenca = Subtrair(Mira_x, Mira_y, Mira_z, Jogador);
+	TPersonagem Angulo;
+	Angulo.z = (-(float)atan2(diferenca.x, diferenca.y)) / M_PI * 180.0f + 180.0f;
+	return Angulo;
+}
+
+void InitBalas(TProjeteis balas[], int tamanho)
+{
+	for(int i = 0; i<tamanho; i++)
+	{
+		balas[i].ID = PROJETIL;
+		balas[i].velocidade = 10;
+		balas[i].ativo = false;
+	}
+}
+void AtiraBalas(TProjeteis balas[], int tamanho, TPersonagem jogador)
+{
+	for(int i = 0; i<tamanho; i++)
+	{
+		if(!balas[i].ativo)
+		{
+			balas[i].x = jogador.x+17;
+			balas[i].y = jogador.y;
+			balas[i].ativo = true;
+			break;
+		}
+	}
+}
+void AtualizarBalas(TProjeteis balas[], int tamanho)
+{
+	for(int i = 0; i<tamanho; i++)
+	{
+		if(balas[i].ativo)
+		{
+			balas[i].x += balas[i].velocidade;
+			if(balas[i].x > LARGURA_TELA)
+				balas[i].ativo = false;
+		}
+	}
+}
+void DesenharBalas(BITMAP *buffer, TProjeteis balas[], int tamanho)
+{
+	for(int i = 0; i<tamanho; i++)
+	{
+		if(balas[i].ativo)
+		{
+			circle(buffer, balas[i].x+35, balas[i].y+35, 1, makecol(255,255,255));
+		}
+	}
+}
+
+// Funções Timer;
+void incrementa_TimerOuvirPassos()
+{
+	TimerOuvirPassos++;
+}
+END_OF_FUNCTION(incrementa_TimerOuvirPassos)
+
+void incrementa_TimerTiros()
+{
+	TimerTiros++;
+}
+END_OF_FUNCTION(incrementa_TimerTiros)
 
 int main(void)
 {
@@ -107,34 +203,54 @@ int main(void)
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, ALTURA_TELA, LARGURA_TELA, 0, 0);
 	set_window_title("Counter Strike 2D");
 	install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, NULL);
+
+	// INICIALIZAÇÃO DE OBJETOS
+	TPersonagem Jogador;
+	TProjeteis Balas[NUM_BALAS];
+	InitBalas(Balas, NUM_BALAS);
+
 	// Variaveis
-	int x = 100, y = 100, vel = 5;
+	Jogador.x = 100; 
+	Jogador.y = 100;
+	int vel = 5;
 	bool TocandoPassos = false;
-	timer = 0;
-	angulo=0;
-	LOCK_FUNCTION(incrementa_timer);
-	LOCK_VARIABLE(timer);
-	install_int_ex(incrementa_timer, MSEC_TO_TIMER(250));
+	bool TocandoTiros = false;
+	TimerOuvirPassos = 0;
+	TimerTiros = 0;
+	LOCK_FUNCTION(incrementa_TimerOuvirPassos);
+	LOCK_FUNCTION(incrementa_TimerTiros);
+	LOCK_VARIABLE(TimerOuvirPassos);
+	LOCK_VARIABLE(TimerTiros);
+	install_int_ex(incrementa_TimerOuvirPassos, MSEC_TO_TIMER(250));
+	install_int_ex(incrementa_TimerTiros, MSEC_TO_TIMER(80));
+
 	// Icone do Jogo
 
 	// Carregar Imagens
 	BITMAP *buffer = create_bitmap(ALTURA_TELA,LARGURA_TELA);
-	BITMAP *Armas[5];
-	// Quatro lados do Personagem
 	BITMAP *Personagem = load_bitmap("Imagens/Jogadores/player_c.bmp", NULL);
 	BITMAP *Mira = load_bitmap("Imagens/Outros/Mira.bmp", NULL);
+	
+	//Carregar a mira
 	show_mouse(Mira);
+
 	// MAPA
+
 	int linhas, colunas;
 	int **mapa = Carregar_Mapa("Mapas/mapa.txt", &linhas, &colunas);
+	
 	//Sons
+	
 	SAMPLE *Caminhar = load_sample("Sons/Jogadores/Correndo.wav");
+	SAMPLE *STiro = load_sample("Sons/Jogadores/Tiro.wav");
+	
 	// Looping do Jogo
+
 	while(!key[KEY_ESC])
 	{
 		if(key[KEY_D] && !key[KEY_LSHIFT]) // Direita
 		{
-			x += 2;
+			Jogador.x += 2;
 			//			Var       Vol  Can  Vel Rep
 			if(!TocandoPassos)
 			{
@@ -143,11 +259,11 @@ int main(void)
 		}
 		else if(key[KEY_D] && key[KEY_LSHIFT]) // Direita
 		{
-			x += 1;
+			Jogador.x += 1;
 		}
 		if(key[KEY_A] && !key[KEY_LSHIFT]) // Esquerda
 		{ 
-			x -= 2;
+			Jogador.x -= 2;
 			if(!TocandoPassos)
 			{
 				TocandoPassos = true;
@@ -155,11 +271,11 @@ int main(void)
 		}
 		else if(key[KEY_A] && key[KEY_LSHIFT]) // Esquerda
 		{ 
-			x -= 1;
+			Jogador.x -= 1;
 		}
 		if(key[KEY_W] && !key[KEY_LSHIFT]) // Cima
 		{ 
-			y -= 2;
+			Jogador.y -= 2;
 			if(!TocandoPassos)
 			{
 				TocandoPassos = true;
@@ -167,11 +283,11 @@ int main(void)
 		}
 		else if(key[KEY_W] && key[KEY_LSHIFT]) // Cima
 		{ 
-			y -= 1;
+			Jogador.y -= 1;
 		}
 		if(key[KEY_S] && !key[KEY_LSHIFT]) // Baixo
 		{
-			y += 2;
+			Jogador.y += 2;
 			if(!TocandoPassos)
 			{
 				TocandoPassos = true;
@@ -179,29 +295,50 @@ int main(void)
 		}
 		else if(key[KEY_S] && key[KEY_LSHIFT])
 		{
-			y += 1;
+			Jogador.y += 1;
 		}
-		if(timer > 1)
+		if(TimerOuvirPassos > 1)
 		{
 			if(key[KEY_W] || key[KEY_A] || key[KEY_D] || key[KEY_S])
 			{
 				if(!key[KEY_LSHIFT])
 				{
 					play_sample(Caminhar, 255, 128, 1000, 0);
-					timer = 0;
+					play_sample(Caminhar, 255, 128, 1000, 0);
+					TimerOuvirPassos = 0;
 				}
 				else
 				{
 					TocandoPassos = false;
-					timer = 0;
+					TimerOuvirPassos = 0;
 				}
 			}
 		}
+		if(TimerTiros > 1)
+		{
+			if(TocandoTiros)
+			{
+				TocandoTiros = false;
+				TimerTiros = 0;
+			}
+		}
+		if(key[KEY_SPACE])
+		{
+			if(!TocandoTiros)
+			{	
+				play_sample(STiro, 255, 128, 1000, 0);
+				AtiraBalas(Balas, NUM_BALAS, Jogador);
+				TocandoTiros =  true;
+			}
+		}
+		AtualizarBalas(Balas, NUM_BALAS);
 		Desenhar_Mapa(buffer, mapa, linhas, colunas);
 		//draw_sprite(buffer, Personagem, 100+x, 100+y);
-		pivot_sprite(buffer, Personagem, 100+x, 100+y, 100, 100, itofix(GRAUS_PARA_ALLEGRO(mouse_y)));
-		//rotate_sprite(buffer, Personagem, mouse_x, mouse_y, itofix(GRAUS_PARA_ALLEGRO(angulo)));
+		//pivot_sprite(buffer, Personagem, Jogador.x, Jogador.y, 0, 0, itofix(GRAUS_PARA_ALLEGRO(Angulo.z)));
+		TPersonagem Angulo = CalcularAngulo(mouse_x, mouse_y, mouse_z, Jogador);
+		rotate_sprite(buffer, Personagem, Jogador.x, Jogador.y, itofix(GRAUS_PARA_ALLEGRO(Angulo.z)));
 		draw_sprite(buffer, Mira, mouse_x, mouse_y);
+		DesenharBalas(buffer, Balas, NUM_BALAS);
 		draw_sprite(screen, buffer, 0, 0);
 		rest(1);
 		clear(buffer);
